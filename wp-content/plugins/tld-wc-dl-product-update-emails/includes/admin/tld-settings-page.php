5e51a4bc30cc23df8066837df61e6a080fb146b8
TheLoneDeveloper <uriahs.victor@gmail.com>
2016-04-25 23:31:35 -0400

    Set default current schedule value

--- wp-content/plugins/tld-wc-dl-product-update-emails/includes/admin/tld-settings-page.php
+++ wp-content/plugins/tld-wc-dl-product-update-emails/includes/admin/tld-settings-page.php
@@ -12,7 +12,7 @@ function tld_wcdpue_add_admin_menu() {
 }
 
 function tld_wcdpue_settings_page() {
-  //page mark up
+  //page html 
   ?>
 
   <div class="wrap">
@@ -37,87 +37,96 @@ function tld_wcdpue_settings_page() {
         <tr valign="top">
           <th scope="row">Message</th>
           <td>
-            <textarea name="tld-wcdpue-email-body" placeholder="Enter optional text for the email body." rows="15" cols="70"/><?php echo esc_attr( get_option('tld-wcdpue-email-body') ); ?>
-          </textarea>
-        </td>
-      </tr>
+            <textarea name="tld-wcdpue-email-body" placeholder="Enter optional text for the email body." rows="15" cols="70"/><?php echo esc_attr( get_option('tld-wcdpue-email-body') ); ?>     </textarea>
+          </td>
+        </tr>
 
-      <tr valign="top">
-        <td><h3>Email Schedule Options</h3></td>
-      </tr>
+        <tr valign="top">
+          <td><h3>Email Schedule Options</h3></td>
+        </tr>
 
-      <tr valign="top">
-        <th scope="row">Send e-mails in bursts of <small>(recommend you keep this value below 10)</small>:</th>
-        <td>
-          <input type="number" name="tld-wcdpue-email-bursts-count" value="<?php echo esc_attr( get_option('tld-wcdpue-email-bursts-count') ); ?>" min="1" size="4"/>
-        </td>
-      </tr>
+        <tr valign="top">
+          <th scope="row">Send e-mails in bursts of <small>(recommend you keep this value below 10)</small>:</th>
+          <td>
+            <input type="number" name="tld-wcdpue-email-bursts-count" value="<?php echo esc_attr( get_option('tld-wcdpue-email-bursts-count') ); ?>" min="1" size="4"/>
+          </td>
+        </tr>
 
-      <tr valign="top">
-        <th scope="row">Select Schedule:</th>
-        <td>
+        <tr valign="top">
+          <th scope="row">Select Schedule:</th>
+          <td>
 
-          <select name="tld-wcdpue-schedule-setting-value">
+            <select name="tld-wcdpue-schedule-setting-value">
+              <?php
+
+              $active_cron_schedules = wp_get_schedules();
+              foreach ( $active_cron_schedules as $key => $value ) {
+
+                echo '<option value="' . $key . '">' . $value['display'] . '</option>';
+
+              }
+
+              ?>
+            </select>
+          </td>
+
+        </tr>
+
+        <tr valign="top">
+          <th>Current Schedule:</th>
+          <td>
             <?php
 
-            $active_cron_schedules = wp_get_schedules();
-            foreach ( $active_cron_schedules as $key => $value ) {
-
-              echo '<option value="' . $key . '">' . $value['display'] . '</option>';
-
-            }
-
-            ?>
-          </select>
-        </td>
-
-      </tr>
-
-      <tr valign="top">
-        <th>Current Schedule:</th>
-        <td>
-          <?php
-
-          foreach ( $active_cron_schedules as $key => $value ) {
-
             $tld_wcdpue_current_schedule = esc_attr( get_option( 'tld-wcdpue-schedule-setting-value' ) );
-            if( $key == $tld_wcdpue_current_schedule ){
 
-              echo  $value['display'];
-              
+            if ( !empty( $tld_wcdpue_current_schedule ) ){
+
+              foreach ( $active_cron_schedules as $key => $value ) {
+
+                //$tld_wcdpue_current_schedule = esc_attr( get_option( 'tld-wcdpue-schedule-setting-value' ) );
+                if( $key == $tld_wcdpue_current_schedule ){
+
+                  echo  $value['display'];
+
+                }
+
+              }
+
+            }else{
+
+              echo "Once Daily";
+
             }
-          }
+            ?>
+          </td>
+        </tr>
+      </table>
 
-          ?>
-        </td>
-      </tr>
-    </table>
+      <?php submit_button(); ?>
 
-    <?php submit_button(); ?>
-
-  </form>
-</div>
+    </form>
+  </div>
 
 
-<?php }
+  <?php }
 
-function tld_wcdpue_settings() {
+  function tld_wcdpue_settings() {
 
-  //register our settings
-  register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-email-subject' );
-  register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-email-body' );
-  register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-email-bursts-count' );
-  register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-schedule-setting-value' );
+    //register our settings
+    register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-email-subject' );
+    register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-email-body' );
+    register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-email-bursts-count' );
+    register_setting( 'tld_wcdpue-settings-group', 'tld-wcdpue-schedule-setting-value' );
 
-}
+  }
 
-function tld_wcdpue_update_schedule() {
+  function tld_wcdpue_update_schedule() {
 
-  $tld_wcdpue_set_cron_interval = get_option( 'tld-wcdpue-schedule-setting-value' ); //get interval set by user
-  wp_clear_scheduled_hook('tld_wcdpue_email_burst'); //remove previous scheduled time
-  wp_schedule_event( time(), $tld_wcdpue_set_cron_interval, 'tld_wcdpue_email_burst' ); //add new scheduled time
+    $tld_wcdpue_set_cron_interval = get_option( 'tld-wcdpue-schedule-setting-value' ); //get interval set by user
+    wp_clear_scheduled_hook('tld_wcdpue_email_burst'); //remove previous scheduled time
+    wp_schedule_event( time(), $tld_wcdpue_set_cron_interval, 'tld_wcdpue_email_burst' ); //add new scheduled time
 
-}
-add_action( 'update_option_tld-wcdpue-schedule-setting-value', 'tld_wcdpue_update_schedule');
+  }
+  add_action( 'update_option_tld-wcdpue-schedule-setting-value', 'tld_wcdpue_update_schedule');
 
-?>
+  ?>
