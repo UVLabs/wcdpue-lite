2b247656ffa14079c61ba94772930302ad56bd91
TheLoneDeveloper <uriahs.victor@gmail.com>
2016-04-25 20:23:21 -0400

    Fixed version number
    Last commit was set as patch release rather than minor release.

--- wp-content/plugins/tld-wc-dl-product-update-emails/tld-wc-dl-product-update-emails.php
+++ wp-content/plugins/tld-wc-dl-product-update-emails/tld-wc-dl-product-update-emails.php
@@ -3,7 +3,7 @@
 Plugin Name: TLD WC Downloadable Product Update Emails
 Plugin URI: http://soaringleads.com
 Description: Inform customers when there is an update to their downloadable product.
-Version: 1.1.1-beta
+Version: 1.2.0-beta
 Author: Uriahs Victor
 Author URI: http://soaringleads.com
 License: GPL2
